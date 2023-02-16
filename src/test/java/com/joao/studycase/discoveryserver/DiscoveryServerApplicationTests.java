e9b1ecf2c76908e7138cc12317b6e71c0e388c38
Joao Vitor <joaovitormoraveis@gmail.com>
2023-02-15 20:57:55 -0500

    first commit

---
+++ src/test/java/com/joao/studycase/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.studycase.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
